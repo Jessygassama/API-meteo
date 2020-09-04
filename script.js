fffdc1782a1154df7b47e9a7e6193d8d6c15e0ff
jessygassama <jessygassama@gmail.com>
2020-09-04 17:57:54 +0200

    modification js + supression jquery

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-// let apiCall = function(city){
-//     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1b934d5090a2b75af63cb46d0640dc24`;
-// fetch(url).then(response =>
-//      response.json().then((data) => {
-//          console.log(data);
-
-//          document.querySelector('#city').innerHTML = data.name;
-//          document.querySelector('#temp').innerHTML ="<i class='fas fa-thermometer-quarter'></i>"+ data.main.temp + '°';
-//          document.querySelector('#humidity').innerHTML = "<i class='fas fa-tint'></i>"+data.main.humidity + '%';
-//          document.querySelector('#wind').innerHTML = "<i class='fas fa-wind'></i>"+data.wind.speed + 'km/h';
-//      })
-// ).catch(err => console.log('Erreur' + err));
-
-// }
-
-// document.querySelector('form').addEventListener('submit',function(e) {
-//     e.preventDefault();
-//     let ville = document.querySelector('#inpucity').value;
-
-//     apiCall(ville);
-// });
-
-
-// apiCall('Paris');
-
-
-    
-
-
 
 let apiCall = function(city){
     let meteo = fetch( `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1b934d5090a2b75af63cb46d0640dc24`)
     .then(response => response.json())
     .then(json =>{
                 const nom = json.name;
-                const temperature =  "<i class='fas fa-water'></i>"+json.main.temp + '°';
+                const temperature =  "<i class='fas fa-thermometer-quarter'></i>"+json.main.temp + '°';
                 const humidity = "<i class='fas fa-tint'></i>"+ json.main.humidity + '%';
                 const wind = ("<i class='fas fa-wind'></i>")+ json.wind.speed + 'km/h';
 
